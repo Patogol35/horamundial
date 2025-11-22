a2ae40af9a8d4400fcc83f44cf220a41afe114b1
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-11-22 09:47:31 -0500

    Remove Quito from cities list

--- src/data/cities.js
+++ src/data/cities.js
@@ -1,5 +1,5 @@
 export const cities = [
-  { name: "Quito", timezone: "America/Guayaquil" },
+  { name: "Ambato", timezone: "America/Guayaquil" },
   { name: "Londres", timezone: "Europe/London" },
   { name: "Nueva York", timezone: "America/New_York" },
   { name: "Tokio", timezone: "Asia/Tokyo" },
