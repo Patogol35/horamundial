cda59e90206287b00d6de7d1b1e902e6b049ae28
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-08-21 13:23:34 -0500

    Update timeUtils.js

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -1,33 +1,26 @@
 export function formatTime(date, timezone) {
-  return new Intl.DateTimeFormat("es-ES", {
-    timeZone: timezone,
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: true,
-  }).format(date);
-}
-
-export function getHourInTimezone(
-  date,
-  timezone
-) {
-  const hour = new Intl.DateTimeFormat(
-    "en-US",
+  return new Intl.DateTimeFormat(
+    "es-ES",
     {
       timeZone: timezone,
       hour: "2-digit",
-      hour12: false,
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
     }
   ).format(date);
-
-  return Number(hour);
 }
 
 export function isDaytime(date, timezone) {
-  const hour = getHourInTimezone(
-    date,
-    timezone
+  const hour = Number(
+    new Intl.DateTimeFormat(
+      "en-US",
+      {
+        timeZone: timezone,
+        hour: "2-digit",
+        hour12: false,
+      }
+    ).format(date)
   );
 
   return hour >= 6 && hour < 18;
