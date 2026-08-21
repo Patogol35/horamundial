c31f06c0c22fb858a9472855cc76d31cfebf2c50
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-08-21 12:40:36 -0500

    Update cities.js

--- src/data/cities.js
+++ src/data/cities.js
@@ -1,17 +1,50 @@
 export const cities = [
-  { name: "Quito", timezone: "America/Guayaquil" },
-  { name: "Londres", timezone: "Europe/London" },
-  { name: "Nueva York", timezone: "America/New_York" },
-  { name: "Tokio", timezone: "Asia/Tokyo" },
-  { name: "Sídney", timezone: "Australia/Sydney" },
-  { name: "Madrid", timezone: "Europe/Madrid" },
-  { name: "París", timezone: "Europe/Paris" },
-  { name: "Los Ángeles", timezone: "America/Los_Angeles" },
-  { name: "Ciudad de México", timezone: "America/Mexico_City" },
+  {
+    name: "Quito",
+    timezone: "America/Guayaquil",
+  },
+  {
+    name: "Londres",
+    timezone: "Europe/London",
+  },
+  {
+    name: "Nueva York",
+    timezone: "America/New_York",
+  },
+  {
+    name: "Tokio",
+    timezone: "Asia/Tokyo",
+  },
+  {
+    name: "Sídney",
+    timezone: "Australia/Sydney",
+  },
+  {
+    name: "Madrid",
+    timezone: "Europe/Madrid",
+  },
+  {
+    name: "París",
+    timezone: "Europe/Paris",
+  },
+  {
+    name: "Los Ángeles",
+    timezone: "America/Los_Angeles",
+  },
+  {
+    name: "Ciudad de México",
+    timezone: "America/Mexico_City",
+  },
   {
     name: "Buenos Aires",
     timezone: "America/Argentina/Buenos_Aires",
   },
-  { name: "Toronto", timezone: "America/Toronto" },
-  { name: "Roma", timezone: "Europe/Rome" },
+  {
+    name: "Toronto",
+    timezone: "America/Toronto",
+  },
+  {
+    name: "Roma",
+    timezone: "Europe/Rome",
+  },
 ];
