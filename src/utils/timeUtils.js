f30d870fff2b4272319953aa039da2e569805512
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-08-21 12:17:36 -0500

    Add time utility functions for formatting and checks

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -1,1 +1,25 @@
+export function formatTime(date, timezone) {
+  return new Intl.DateTimeFormat("es-ES", {
+    timeZone: timezone,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  }).format(date);
+}
 
+export function getHourInTimezone(date, timezone) {
+  const hour = new Intl.DateTimeFormat("en-US", {
+    timeZone: timezone,
+    hour: "2-digit",
+    hour12: false,
+  }).format(date);
+
+  return Number(hour);
+}
+
+export function isDaytime(date, timezone) {
+  const hour = getHourInTimezone(date, timezone);
+
+  return hour >= 6 && hour < 18;
+    }
